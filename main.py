49190a67f5e608c24458d050304a013c84087b6a
moaiweishui <liuyuanxiu1218@gmail.com>
2017-07-10 15:20:06 +0800

    Update main.py.

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 from content_parsing_tool import *
+from sina_weibo import *
 
 filename = raw_input('请输入文件名:\n')
 try:
     f = open(filename, 'r')
     content = f.read()
-    basic_info = get_basic_info(content)
-    for key, values in basic_info.items():
-        print key + ':' + values
-    print type(basic_info['page_num']), int(basic_info['page_num'])
+    user_weibo = sina_weibo('1523456657', 'weibo.cn', content)
 finally:
     f.close()
+
+user_weibo.get_basic_info()
+user_weibo.display_basic_info()
