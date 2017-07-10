12de9c64ffd082c7603439166e1ddcf5b5eca594
moaiweishui <liuyuanxiu1218@gmail.com>
2017-07-10 21:58:12 +0800

    Update main.py.

--- main.py
+++ main.py
@@ -5,14 +5,56 @@ sys.setdefaultencoding("utf-8")
 
 from content_parsing_tool import *
 from sina_weibo import *
+from wap_weibo_crawler import *
 
+'''
 filename = raw_input('请输入文件名:\n')
 try:
     f = open(filename, 'r')
     content = f.read()
-    user_weibo = sina_weibo('1523456657', 'weibo.cn', content)
+    user = sina_weibo('1523456657')
+    user.get_basic_info(content)
 finally:
     f.close()
 
-user_weibo.get_basic_info()
-user_weibo.display_basic_info()
+user.display_basic_info()
+
+user.get_weibo_content(filename)
+#user.display_weibo_content()
+user.save2markdown()
+'''
+
+
+username = raw_input('请输入用户名:\n')
+password = raw_input('请输入密码：\n')
+weibo_crawler = WapWeiboCrawler(username, password)
+weibo_crawler.log_in()
+
+user_id = '1523456657'
+user_id = '1654301217'
+user = sina_weibo(user_id)
+home_page = weibo_crawler.get_homepage(user.user_id)
+user.get_basic_info(home_page)
+user.display_basic_info()
+
+page_num = raw_input('请输入需要获取的页数：')
+filename = weibo_crawler.get_content(user.user_id, int(page_num))
+
+user.get_weibo_content(filename)
+#user.display_weibo_content()
+user.save2markdown()
+
+
+
+
+'''
+weibo_list = get_weibo_content('wjy1.txt')
+for weibo in weibo_list:
+    print '\n----------------------------------------'
+    print weibo['cnt'], weibo['time']
+    print weibo['content']
+    print weibo['attitude'] + '  ',
+    print weibo['repost'] + '  ',
+    print weibo['comment'] + '  '
+    print '\n\n'
+'''
