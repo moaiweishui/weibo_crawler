3566a25932dcfff846675d6bf2c3fec15b4bb0ad
moaiweishui <liuyuanxiu1218@gmail.com>
2017-07-11 11:19:55 +0800

    Add DEBUG switch

--- main.py
+++ main.py
@@ -7,43 +7,46 @@ from content_parsing_tool import *
 from sina_weibo import *
 from wap_weibo_crawler import *
 
-'''
-filename = raw_input('请输入文件名:\n')
-try:
-    f = open(filename, 'r')
-    content = f.read()
-    user = sina_weibo('1523456657')
-    user.get_basic_info(content)
-finally:
-    f.close()
+DEBUG = True
 
-user.display_basic_info()
+if DEBUG:
+    filename = raw_input('请输入文件名:\n')
+    filename = 'output file/' + filename
+    try:
+        f = open(filename, 'r')
+        content = f.read()
+        user = sina_weibo('1523456657')
+        user.get_basic_info(content)
+    finally:
+        f.close()
 
-user.get_weibo_content(filename)
-#user.display_weibo_content()
-user.save2markdown()
-'''
+    user.display_basic_info()
 
-print '\n\n' + '-'*40 + '\n'
-username = raw_input('请输入用户名:\n')
-password = raw_input('请输入密码：\n')
-print '\n'
-weibo_crawler = WapWeiboCrawler(username, password)
-weibo_crawler.log_in()
+    user.get_weibo_content(filename)
+    #user.display_weibo_content()
+    user.save2markdown()
 
-user_id = '1523456657'
-user_id = '1654301217'
-user = sina_weibo(user_id)
-home_page = weibo_crawler.get_homepage(user.user_id)
-user.get_basic_info(home_page)
-user.display_basic_info()
+else:
+    print '\n\n' + '-'*40 + '\n'
+    username = raw_input('请输入用户名:\n')
+    password = raw_input('请输入密码：\n')
+    print '\n'
+    weibo_crawler = WapWeiboCrawler(username, password)
+    weibo_crawler.log_in()
 
-page_num = raw_input('请输入需要获取的页数：')
-filename = weibo_crawler.get_content(user.user_id, user.basic_info['username'], int(page_num))
+    user_id = '1523456657'
+    user_id = '1654301217'
+    user = sina_weibo(user_id)
+    home_page = weibo_crawler.get_homepage(user.user_id)
+    user.get_basic_info(home_page)
+    user.display_basic_info()
 
-user.get_weibo_content(filename)
-#user.display_weibo_content()
-user.save2markdown()
+    page_num = raw_input('请输入需要获取的页数：')
+    filename = weibo_crawler.get_content(user.user_id, user.basic_info['username'], int(page_num))
+
+    user.get_weibo_content(filename)
+    #user.display_weibo_content()
+    user.save2markdown()
 
 
 
