fb1ecc911fedce57acf84f70263f1f7a7ee4b1e4
moaiweishui <liuyuanxiu1218@gmail.com>
2017-09-11 23:11:57 +0800

    Update seed_user_db.py

--- seed_user_db.py
+++ seed_user_db.py
@@ -9,9 +9,12 @@ sys.setdefaultencoding("utf-8")
 
 
 # Create seed user table
-def create_seed_user_table(table_name):
+def create_seed_user_table(table_name, is_drop_old_table=False):
     db = MySQLdb.connect("localhost", "root", "123456", "weibo_crawler")
     cursor = db.cursor()
+    if is_drop_old_table:
+        cursor.execute('DROP TABLE IF EXISTS %s' % table_name)
+        db.commit()
     sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = 'weibo_crawler' and TABLE_NAME = '%s'" % table_name
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -29,6 +32,7 @@ def create_seed_user_table(table_name):
             db.commit()
             print 'Create table \'%s\' succeed' % table_name
         except Exception, e:
+            db.rollback()
             print 'Error occurs while creating table \'%s\'' % table_name
             traceback.print_exc()
             print traceback.format_exc()
@@ -45,22 +49,72 @@ def insert_user_to_seed_table(table_name, user_id):
         print 'User %s already exists in table %s' % (user_id, table_name)
     else:
         sql = "INSERT INTO %s(user_id) VALUES (\'%s\')" % (table_name, user_id)
-        cursor.execute(sql)
-        db.commit()
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except Exception, e:
+            db.rollback()
+            print 'Error occurs while inserting data'
+            traceback.print_exc()
+            print traceback.format_exc()
     db.close()
 
-
-'''
-sql = "SELECT uid FROM seed_user"
-
-try:
+# Update user in seed_user table
+def update_user_in_seed_table(table_name, user_id, update_time):
+    db = MySQLdb.connect("localhost", "root", "123456", "weibo_crawler")
+    cursor = db.cursor()
+    sql = "SELECT * FROM %s where user_id = \'%s\'" % (table_name, user_id)
     cursor.execute(sql)
     result = cursor.fetchall()
-    for uid in result:
-        print uid
-except:
-    print "Error: unable to fetch data"
-'''
+    if not result:
+        print 'User %s does not exists in table %s' % (user_id, table_name)
+    else:
+        sql = "UPDATE %s SET update_time = \'%s\' WHERE user_id = \'%s\'" % (table_name, update_time, user_id)
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except Exception, e:
+            db.rollback()
+            print 'Error occurs while updating data'
+            traceback.print_exc()
+            print traceback.format_exc()
+    db.close()
+
+# Get seed user id
+# only_new:  true: have not been crawled
+#           false: all of the seed users
+def get_seed_user(table_name, only_new=True):
+    seed_user_list = []
+    db = MySQLdb.connect("localhost", "root", "123456", "weibo_crawler")
+    cursor = db.cursor()
+    sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = 'weibo_crawler' and TABLE_NAME = '%s'" % table_name
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if not result:
+        print 'Table %s not exists' % table_name
+    else:
+        if only_new:
+            sql = 'SELECT user_id FROM %s WHERE update_time IS NULL' % table_name
+        else:
+            sql = 'SELECT user_id FROM %s' % table_name
+        try:
+            cursor.execute(sql)
+            seed_user_list = cursor.fetchall()
+            db.commit()
+        except Exception, e:
+            db.rollback()
+            print 'Error occurs while updating data'
+            traceback.print_exc()
+            print traceback.format_exc()
+    db.close()
+    return seed_user_list
+
+
 if __name__ == '__main__':
-    #create_seed_user_table('seed_user')
-    insert_user_to_seed_table('seed_user', '12345')
+    create_seed_user_table('seed_user')
+    insert_user_to_seed_table('seed_user', '')
+    insert_user_to_seed_table('seed_user', '')
+    #update_user_in_seed_table('seed_user', '12345', '2017-9-13')
+    seed_user_list = get_seed_user('seed_user')
+    for x in seed_user_list:
+        print x
